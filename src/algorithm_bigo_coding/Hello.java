6cf2248916fd153cf10970e7c27816ddcca965dc
thelinh <truongthelinh95@gmail.com>
2025-12-18 14:56:08 +0700

    first commit.

---
+++ src/algorithm_bigo_coding/Hello.java
@@ -0,0 +1,7 @@
+package algorithm_bigo_coding;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.print("hello");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module algorithm_bigo_coding {
+}
